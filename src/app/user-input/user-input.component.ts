fa4672d9843d405bd12af4860c5b501d6a516280
annaivv <annashalya@gmail.com>
2025-02-03 21:52:26 +0100

    feature: user input form with 2-way-binding added

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
-  styleUrl: './user-input.component.css'
+  styleUrl: './user-input.component.css',
 })
 export class UserInputComponent {
+  enteredInitial = '0';
+  enteredAnnual = '0';
+  enteredReturn = '5';
+  enteredPeriod = '10';
+  formSubmitted = false;
 
+  onSubmit() {
+    console.log('Submitted!');
+    console.log(this.enteredInitial);
+    this.formSubmitted = true;
+  }
 }
